22cb9403718f14ead116f942d4e954dc5b490c30
Hitesh <hitesh164722@gmail.com>
2026-03-04 20:06:14 +0530

    Simple publisher in Python node

---
+++ src/espbot_py_examples/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'espbot_py_examples'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hitesh',
+    maintainer_email='hitesh164722@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'simple_publisher = espbot_py_examples.simple_publisher:main'
+        ],
+    },
+)
